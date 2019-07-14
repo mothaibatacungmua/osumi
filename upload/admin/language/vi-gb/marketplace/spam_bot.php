27f551fb07fad73cbc2729eb4f80c8f63471fb4f
ducta <duc.tran3@tiki.vn>
2019-07-15 00:57:32 +0700

    Add vietnamese language

---
+++ upload/admin/language/vi-gb/marketplace/spam_bot.php
@@ -0,0 +1,10 @@
+<?php
+// Heading
+$_['heading_title']        = 'Spam Bot';
+
+// Text
+$_['text_instruction']     = 'Spam Bot Instructions';
+$_['text_spam_bot']         = 'Bot spam to target users include phone, email, or facebook by uploading a .csv file';
+
+$_['entry_message']        = 'Hi guys,...';
+$_['entry_subject']        = 'Subject';
